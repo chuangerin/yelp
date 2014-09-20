8b49f8a0c744b723173e8cc21bac1ca76666334a
Erin Chuang <chuangerin@gmail.com>
2014-09-20 14:40:20 +0800

    Initial Commit

---
+++ yelp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yelp
+//
+//  Created by Erin Chuang on 9/20/14.
+//  Copyright (c) 2014 Erin Chuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
